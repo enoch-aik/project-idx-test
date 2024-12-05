7934eda148f9ad54210ab4aa888d8ecc358e9f04
Enoch <enoch_aik@icloud.com>
2024-12-06 00:20:03 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "project-idx-test"
+include(":android")
+include(":shared")
